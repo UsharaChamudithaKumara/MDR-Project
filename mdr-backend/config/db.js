240d8b2a3bc32f029c33cd404f90c45d7d7f70e8
Ushara Chamuditha Kumara <usharakumara20030820@gmail.com>
2026-03-23 15:28:20 +0530

    update

--- mdr-backend/config/db.js
+++ mdr-backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || process.env.DB_PASS || "root",
+  password: process.env.DB_PASSWORD || process.env.DB_PASS || "1234",
   database: process.env.DB_NAME || "mdr_system"
 });
 
